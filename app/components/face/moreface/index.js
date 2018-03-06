79ef1d635f56b275a1247f80820d90b225e86060
panjianbo <13651030816@163.com>
2018-03-06 14:48:32 +0800

    ss

--- app/components/face/moreface/index.js
+++ app/components/face/moreface/index.js
@@ -6,51 +6,51 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  DeviceEventEmitter
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	DeviceEventEmitter
 } from 'react-native';
 
 export default class Alarm extends Component {
-			render() {
-				return (
-					<View>
-						<View style={styles.panel}>
-							<Image source={require('../../../../resources/pz.png')} style={{height:160,width:160,marginTop:40}}/>
-						</View>
-					</View>
-					)
-			}
+	render() {
+		return (
+			<View>
+				<View style={styles.panel}>
+					<Image source={require('../../../../resources/pz.png')} style={{ height: 130, width: 130, marginTop: 30 }} />
+				</View>
+			</View>
+		)
 	}
-    
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#F5FCFF',
-				},
-				panel:{
-					flex:1,
-					height:60,
-					alignItems: 'center',
-				},
-        welcome: {
-            fontSize: 20,
-            textAlign: 'center',
-            margin: 10,
-        },
-        instructions: {
-            marginTop:10,
-            textAlign: 'center',
-            color: '#333333',
-            marginBottom: 5,
-            fontSize:18
-        },
-    });
-    
-    
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	panel: {
+		flex: 1,
+		height: 60,
+		alignItems: 'center',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		marginTop: 10,
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+		fontSize: 18
+	},
+});
+
+
 
